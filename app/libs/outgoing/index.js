0bb3e17b594f9454bf8e2aac82012854d2f965c6
Eyni, Kave <40831732+dewebdes@users.noreply.github.com>
2024-09-17 08:51:57 +0330

    worker request

--- app/libs/outgoing/index.js
+++ app/libs/outgoing/index.js
@@ -17,3 +17,24 @@ function doRequest(durl, dqs) {
 	});
 }
 module.exports.doRequest = doRequest;
+
+function doRequest_proxy(durl) {
+	return new Promise(function (resolve, reject) {
+		console.log(durl);
+
+		var uri2 = "https://flat-dust-acfXXXXXXX.workers.dev/?dieuri=" + encodeURIComponent(durl);
+
+		global.request({ url: uri2 }, function (error, res, body) {
+			console.log('res: ' + res);
+			console.log('err: ' + error);
+			resolve(body);
+			//if (!error && res.statusCode === 200) {
+			//	resolve(body);
+			//} else {
+			//	reject(error);
+			//}
+		});
+
+	});
+}
+module.exports.doRequest_proxy = doRequest_proxy;
