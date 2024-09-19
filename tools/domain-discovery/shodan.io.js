03a001018cc32bf221c961feb9fbe12c92aa065e
Eyni, Kave <40831732+dewebdes@users.noreply.github.com>
2024-09-20 01:38:41 +0330

    shodan

--- tools/domain-discovery/shodan.io.js
+++ tools/domain-discovery/shodan.io.js
@@ -1,3 +1,4 @@
+//https://www.shodan.io/search?query=ssl%3Agreatplacetowork&page=2&key=api-key
 var dms = [];
 var els = document.querySelectorAll('.text-danger');
 for(var i=0;i<=els.length-1;i++){
