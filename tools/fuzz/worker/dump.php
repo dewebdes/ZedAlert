2f50d16456ce9476106c4609009da935d54875f5
Eyni, Kave <40831732+dewebdes@users.noreply.github.com>
2024-10-03 15:39:49 +0330

    Update dump.php

--- tools/fuzz/worker/dump.php
+++ tools/fuzz/worker/dump.php
@@ -1,4 +1,12 @@
 <?php
 print_r($_REQUEST);
+echo '<hr>';
 print_r(apache_request_headers());
+echo '<hr>';
+print_r($_SERVER['HTTP_COOKIE']);
+echo '<hr>';
+print_r($_SERVER);
+echo '<hr>';
+$entityBody = file_get_contents('php://input');
+print_r($entityBody);
 ?>
