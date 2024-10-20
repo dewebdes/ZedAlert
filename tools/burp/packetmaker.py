f5185dd5491d86c73e57fa0c1e01849d5f1c6e4b
Eyni, Kave <40831732+dewebdes@users.noreply.github.com>
2024-10-20 23:15:24 +0330

    Update packetmaker.py

--- tools/burp/packetmaker.py
+++ tools/burp/packetmaker.py
@@ -1,4 +1,6 @@
 import collections
+import os
+import time
 import xml.etree.ElementTree as ET
 import base64
 
@@ -17,6 +19,9 @@ def base64ToString(b):
     return b.decode("utf-8")
     #return base64.b64decode(b).decode('utf-8')
 
+def current_milli_time():
+    return round(time.time() * 1000)
+
 def start(pt):
     print("search for " + pt)
     white_hosts = ['hiring.monster.com','appsapi.monster.io','tagmgr.monster.com','buy.monster.com','tagmgr.monster.lu','candidatehelp.monster.com','api.monster.io','hiring-identity.monster.com','manage.monster.co.uk','www.monster.lu','identity.monster.com','careers.monster.com','tagmgr.monster.co.uk','www.monster.com','manage.monster.com']
@@ -33,24 +38,29 @@ def start(pt):
         request = item.find('request').text
         response = item.find('response').text
 
-        if str(pt) in str(url):
-            if len(white_hosts) == 0:
-                feed.append(bitem(url, host, method, path, extension, mimetype, request, response))
-            else:
-                if host in white_hosts:
+        if(method != "OPTIONS"):
+            if str(pt) in str(url):
+                if len(white_hosts) == 0:
                     feed.append(bitem(url, host, method, path, extension, mimetype, request, response))
+                else:
+                    if host in white_hosts:
+                        feed.append(bitem(url, host, method, path, extension, mimetype, request, response))
     print(str(len(feed)))
     return feed
 
 var = input("Please enter path: ")
 
 ret = start(var)
-print("\n\nresult:\n" + ret[0].url)
 
-b64 = base64ToString(base64.b64decode(ret[0].request))
-
-print(b64)
-
-f = open("packet.txt", "a")
-f.write(str(b64))
-f.close()
+new_list = sorted(ret, key=lambda x: x.url, reverse=True)
+packetname = "packet" + "-" + str(current_milli_time())
+os.makedirs(packetname)
+indx = 0
+for item in new_list:
+    b64 = base64ToString(base64.b64decode(item.request))
+    packetname2 = "packet" + "-" + str(current_milli_time()) + "-" + str(indx) + ".txt"
+    f = open(packetname + "/" + packetname2, "a")
+    f.write(str(b64))
+    f.close()
+    print("\n\n" + str(indx) + ": packet write to " + packetname + "/" + packetname2)
+    indx = indx + 1
